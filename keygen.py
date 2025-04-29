9a751e24e06ea3763f78e63d23bd4e895d495a12
ezloomdev <89234023+gmrrz@users.noreply.github.com>
2025-04-28 21:41:30 -0400

    Delete keygen.py

--- keygen.py
+++
@@ -1,8 +0,0 @@
-from cryptography.fernet import Fernet
-
-key = Fernet.generate_key()
-
-with open("key.key", "wb") as key_file:
-    key_file.write(key)
-
-print("Key generated and saved to key.key")
